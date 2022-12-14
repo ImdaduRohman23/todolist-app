a9de88fcb9b8cedf5c9796160f34ba27ff73f223
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-12-15 06:48:32 +0700

    handle submit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 import list from '../../assets/list.gif';
 
 const Header = () => {
+    const [input, setInput] = useState('');
+
+    const handleSubmit = () => {
+        console.log(input)
+    };
+
     return (
         <div className="header">
             <h1 className="header__title">My Todo's</h1>
             <div className="header__content">
                 <h3 className="header__content-moto">"Write it and do it!"</h3>
-                <form className="header__content-form">
-                        <input className='form__input' placeholder='Add todo . . .' type="text" />
+                <form onSubmit={handleSubmit} className="header__content-form">
+                        <input value={input} onChange={(e) => setInput(e.target.value)} className='form__input' placeholder='Add todo . . .' type="text" />
                         <button className='form__button' type='submit'>+</button>
                 </form>
                 <h6 className='form__buttom'>Become focused, organized, and calm. </h6>
